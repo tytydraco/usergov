1d712963a8665c1777c2d2abdbf9a9bd004f9ff7
Tyler Nijmeh <tylernij@gmail.com>
2018-07-08 19:21:53 +0000

    alg: fix cluster support to use different freq tables
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- alg.c
+++ alg.c
@@ -15,12 +15,12 @@ void setup_tunables() {
 
 void prepare() {
 	set_gov(0, "userspace");
-
-	freq_list = get_cpu_freqs();
-	freq_list_size = sizeof(freq_list) + 1;
 }
 
 void algorithm() {	
+	freq_list = get_cpu_freqs();
+	freq_list_size = sizeof(freq_list) + 1;
+
 	int load = get_cpu_load();
 	int index = freq_list_size * (load / 100.0);
 	printf("%d * (%d / %d) = index freq %d\n", freq_list_size, load, 100, index);
